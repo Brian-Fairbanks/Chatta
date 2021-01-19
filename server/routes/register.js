69a1dbf1ecc93744c7025985c1450abd316365b4
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2021-01-19 14:43:04 -0600

    created /register (post) - returns 201 on success - validates post must have username, password, lastname, and first name - validates username is unique - validates password is over 6 characters

--- server/routes/register.js
+++ server/routes/register.js
@@ -4,9 +4,21 @@ const router = express.Router()
 const db = require("../models");
 // const passport = require("../config/passport");
 
-router.post("/", function (req, res) {
+router.post("/", function(req, res) {
   console.log(req.body);
-  res.status(200).send({message:"post should work here!"})
-})
+  db.User.create({
+    username: req.body.username,
+    password: req.body.password,
+    lastName: req.body.lastName,
+    firstName: req.body.firstName
+  })
+    .then(function () {
+      res.json(201, res);
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.status(401).json(err);
+    });
+});
 
 module.exports = router
