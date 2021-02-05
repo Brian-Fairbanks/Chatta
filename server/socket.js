34a0bfa672d49754dad8e619665e840dbf1ba410
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2021-02-05 15:10:52 -0600

    created a dictionary of all users with their connections - stored as an array, so that a user can be signed in simultaniously on multiple devices

--- server/socket.js
+++ server/socket.js
@@ -1,5 +1,8 @@
 var socketio = require("socket.io");
 
+// keep an array of users online
+const socketUserDict = {};
+
 module.exports.Create = function (server) {
   io = socketio(server);
 
@@ -11,12 +14,34 @@ module.exports.Create = function (server) {
     socket.on("setUser", (user) => {
       socket.userID = user._id;
       socket.username = user.username;
-      console.log(socket.username, "has disconnected");
+      console.log(socket.username, "has connected");
+      addClientToMap(user._id, socket.id);
+      console.log(socketUserDict);
     });
 
     // disconnection
     socket.on("disconnect", () => {
       console.log(socket.username, "has disconnected");
+      removeClientFromMap(socket.userID, socket.id);
+      console.log(socketUserDict);
     });
   });
 };
+
+function addClientToMap(userID, socketID) {
+  if (socketUserDict[userID]) {
+    socketUserDict[userID].push(socketID);
+  } else {
+    socketUserDict[userID] = [socketID];
+  }
+}
+
+function removeClientFromMap(userID, socketID) {
+  try {
+    socketUserDict[userID] = socketUserDict[userID].filter(
+      (connection) => connection != socketID
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
